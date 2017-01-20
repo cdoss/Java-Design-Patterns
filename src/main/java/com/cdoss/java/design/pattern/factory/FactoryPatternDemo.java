b6788583ec66788e3040bb32e9d66a5ff2d0aa88
Chandy Doss <chandy.doss@gmail.com>
2017-01-19 22:57:16 -0500

    Created ShapeFactory

---
+++ src/main/java/com/cdoss/java/design/pattern/factory/FactoryPatternDemo.java
@@ -0,0 +1,17 @@
+package com.cdoss.java.design.pattern.factory;
+
+public class FactoryPatternDemo {
+
+	public static void main(String[] args){
+		ShapeFactory shapeFactory = new ShapeFactory();
+		
+		IShape shape = shapeFactory.getShape("CIRCLE");
+		shape.draw();
+		
+		IShape shape2 = shapeFactory.getShape("RECTANGLE");
+		shape2.draw();
+		
+		IShape shape3 = shapeFactory.getShape("SQUARE");
+		shape3.draw();
+	}
+}
---
+++ src/main/java/com/cdoss/java/design/pattern/factory/ShapeFactory.java
@@ -0,0 +1,25 @@
+package com.cdoss.java.design.pattern.factory;
+
+import com.cdoss.java.design.pattern.factory.IShape;
+import com.cdoss.java.design.pattern.factory.shape.Circle;
+import com.cdoss.java.design.pattern.factory.shape.Rectangle;
+import com.cdoss.java.design.pattern.factory.shape.Square;
+
+public class ShapeFactory {
+
+	public IShape getShape(String shapeType){
+		if(shapeType == null){
+			return null;
+		}
+		
+		if(shapeType.equalsIgnoreCase("CIRCLE")){
+			return new Circle();
+		} else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+			return new Rectangle();
+		} else if(shapeType.equalsIgnoreCase("SQUARE")){
+			return new Square();
+		}
+		
+		return null;
+	}
+}
