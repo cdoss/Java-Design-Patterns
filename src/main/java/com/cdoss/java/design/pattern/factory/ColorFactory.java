8edfe9fea91676157285f80b21eacc027071315c
Chandy Doss <chandy.doss@gmail.com>
2017-01-21 08:34:59 -0500

    New color factory that is subclass of abstract factory

---
+++ src/main/java/com/cdoss/java/design/pattern/factory/ColorFactory.java
@@ -0,0 +1,32 @@
+package com.cdoss.java.design.pattern.factory;
+
+import com.cdoss.java.design.pattern.factory.color.Blue;
+import com.cdoss.java.design.pattern.factory.color.Green;
+import com.cdoss.java.design.pattern.factory.color.Red;
+
+public class ColorFactory extends AbstractFactory {
+
+	@Override
+	IShape getShape(String shape) {
+		return null;
+	}
+
+	@Override
+	IColor getColor(String color) {
+		
+		if (color == null) {
+			return null;
+		}
+
+		if (color.equalsIgnoreCase("RED")) {
+			return new Red();
+		} else if (color.equalsIgnoreCase("GREEN")) {
+			return new Green();
+		} else if (color.equalsIgnoreCase("BLUE")) {
+			return new Blue();
+		}
+
+		return null;
+	}
+
+}
