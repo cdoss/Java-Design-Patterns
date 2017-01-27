73805c3841fa54553fed7df23c5cc4f909202d7d
Chandy Doss <chandy.doss@gmail.com>
2017-01-27 00:40:57 -0500

    Adding interfaces for builder pattern

---
+++ src/main/java/com/cdoss/java/design/pattern/builder/Item.java
@@ -0,0 +1,7 @@
+package com.cdoss.java.design.pattern.builder;
+
+public interface Item {
+	public String name();
+	public Packing packing();
+	public float price();
+}
---
+++ src/main/java/com/cdoss/java/design/pattern/builder/Packing.java
@@ -0,0 +1,5 @@
+package com.cdoss.java.design.pattern.builder;
+
+public interface Packing {
+	public String pack();
+}
